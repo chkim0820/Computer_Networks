9797cc9df1bfaeb5194f29d6d9132d89d26ee94f
chkim0820 <chaekim0820@gmail.com>
2023-09-29 14:15:18 -0400

    -u and -d works but needs modification

--- Project 2/Project2.cpp
+++ Project 2/Project2.cpp
@@ -8,19 +8,26 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <tuple>
 using namespace std;
 
 class Project2 {
     private:
-        void optionU();
+        tuple<string, string, string> optionU();
         void optionD();
         void optionQ();
         void optionR();
         void optionO();
 };
 
-void optionU() {
-
+tuple<string, string, string> optionU(char* arg) {
+    string url = arg;
+    string hostname = strtok(arg + 7, "/");
+    string urlFile = url.substr(size(hostname) + 7, string::npos);
+    if (urlFile.empty())
+        urlFile = "/";
+    tuple<string, string, string> result = make_tuple(url, hostname, urlFile);
+    return result;
 }
 
 void optionD(string urlFile, string hostname, string filename) {
@@ -67,17 +74,11 @@ int main(int argc, char* argv[]) {
         else if (0 == strncasecmp(argv[i], "-r", 2))
             rPresent = true;
         else if (0 == strncasecmp(argv[i], "http://",7)){
-            url = arg;
-            // char* tok = strtok(argv[i] + 7, "/");
-            // hostname = tok;
-            hostname = strtok(argv[i] + 7, "/");
-            urlFile = arg.substr(size(hostname) + 7, string::npos);
-            if (urlFile.empty())
-                urlFile = "/";
-
-            // cout << hostname << " is hostname" << endl;
-            // cout << url << " is url" << endl;
-            // cout << urlFile << " is the urlFile" <<endl;
+            uPresent = true;
+            tuple<string, string, string> result = optionU(argv[i]);
+            url = get<0>(result);
+            hostname = get<1>(result);
+            urlFile = get<2>(result);
         }
         else if (0 == strncasecmp(argv[i], "wg",2))// FIX
             filename = "";
@@ -90,11 +91,11 @@ int main(int argc, char* argv[]) {
         cout << "Valid arguments required; ./proj2 -u URL [-d] [-q] [-r] -o filename\n";
         exit(1); // wrong input; try again with 
     }
-
+    
     if (dPresent)
         optionD(urlFile, hostname, filename);
 
-    optionU();
+    
     optionO();
     
     if (qPresent)

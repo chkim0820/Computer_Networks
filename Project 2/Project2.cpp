b7d0dcac8f7395edcd0dbeaa13594ffed1f82deb
chkim0820 <chaekim0820@gmail.com>
2023-09-29 11:30:15 -0400

    -u and -d works finally...

--- Project 2/Project2.cpp
+++ Project 2/Project2.cpp
@@ -4,7 +4,10 @@
  * @brief Completed for CSDS 325 Project 2
  */
 
+#include <cstddef>
+#include <cstring>
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Project2 {
@@ -20,8 +23,10 @@ void optionU() {
 
 }
 
-void optionD() {
-
+void optionD(string urlFile, string hostname, string filename) {
+    cout << "DBG: host: " << hostname << endl;
+    cout << "DBG: web_file: " << urlFile << endl;
+    cout << "DBG: output_file: " << filename << endl;
 }
 
 void optionQ() {
@@ -45,37 +50,53 @@ int main(int argc, char* argv[]) {
     bool rPresent = false;
 
     string url;
+    string urlFile;
+    string hostname;
     string filename;
 
     for (int i = 1; i < argc; i++) {
-        std::string arg = argv[i]; // argument
-        if (arg == "-u")
-            uPresent = i;
-        else if (arg == "-o")
-            oPresent = i;
-        else if (arg == "-d")
-            dPresent = 1;
-        else if (arg == "-q")
-            qPresent = 1;
-        else if (arg == "-r")
-            rPresent = 1;
-        else if (arg.substr(0, 2) == "ht")
+        string arg = argv[i];
+        if (0 == strncasecmp(argv[i], "-u", 2)) // account for diff. cases
+            uPresent = true;
+        else if (0 == strncasecmp(argv[i], "-o", 2))
+            oPresent = true;
+        else if (0 == strncasecmp(argv[i], "-d", 2))
+            dPresent = true;
+        else if (0 == strncasecmp(argv[i], "-q", 2))
+            qPresent = true;
+        else if (0 == strncasecmp(argv[i], "-r", 2))
+            rPresent = true;
+        else if (0 == strncasecmp(argv[i], "http://",7)){
             url = arg;
-        else if (arg.substr(0, 2) == "wg")
-            cout << "place holder";
-        else
-            cout << "Invalid argument " + arg + "\n";
+            // char* tok = strtok(argv[i] + 7, "/");
+            // hostname = tok;
+            hostname = strtok(argv[i] + 7, "/");
+            urlFile = arg.substr(size(hostname) + 7, string::npos);
+            if (urlFile.empty())
+                urlFile = "/";
+
+            // cout << hostname << " is hostname" << endl;
+            // cout << url << " is url" << endl;
+            // cout << urlFile << " is the urlFile" <<endl;
+        }
+        else if (0 == strncasecmp(argv[i], "wg",2))// FIX
+            filename = "";
+        else{
+            cout << "Invalid argument " << argv[i] << "\n";
             exit(1);
+        }
     }
-    if (uPresent == false || oPresent == false || argc < 5) {
+    if (uPresent == false || oPresent == false || argc < 4) {
         cout << "Valid arguments required; ./proj2 -u URL [-d] [-q] [-r] -o filename\n";
         exit(1); // wrong input; try again with 
     }
-    
+
+    if (dPresent)
+        optionD(urlFile, hostname, filename);
+
     optionU();
     optionO();
-    if (dPresent)
-        optionD();
+    
     if (qPresent)
         optionQ();
     if (rPresent)

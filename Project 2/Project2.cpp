675ac5d7b2111eea3219035cfec812a267248e0e
chkim0820 <chaekim0820@gmail.com>
2023-09-29 15:35:06 -0400

    -u and -d working & args processed

--- Project 2/Project2.cpp
+++ Project 2/Project2.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,7 +6,7 @@
 #include <cstring>
 #include <iostream>
 #include <string>
-#include <tuple>
+
 using namespace std;
 
 // Global variables
@@ -14,13 +15,13 @@ int oIndex = -1;
 int dIndex = -1;
 int qIndex = -1;
 int rIndex = -1;
-int urlIndex = -1;
 string url;
 string urlFile;
 string hostname;
 string filename;
 
-void parseArgs(int argc, char* argv[]) {
+int parseArgs(int argc, char* argv[]) {
+    int urlIndex = -1;
     for (int i = 1; i < argc; i++) { // go through all arguments
         char* arg = argv[i];
         if (0 == strncasecmp(arg, "-u", 2)) // account for diff. cases
@@ -35,17 +36,18 @@ void parseArgs(int argc, char* argv[]) {
             rIndex = i;
         else if (0 == strncasecmp(arg, "http://",7))
             urlIndex = i;
-        else if (0 == strncasecmp(arg, "wg",2))// FIX
-            filename = "";
+        else if (i == oIndex + 1)// FIX
+            filename = arg;
         else{
             cout << "Invalid argument " << arg << "\n";
             exit(1);
         }
     }
-    if (uIndex == -1 || oIndex == -1 || argc < 4) {
+    if (uIndex == -1 || oIndex == -1 || argc < 5) { // FIX: check for url and filename, too?
         cout << "Valid arguments required; ./proj2 -u URL [-d] [-q] [-r] -o filename\n";
         exit(1); // wrong input; try again with 
     }
+    return urlIndex;
 }
 
 void optionU(char* arg) {
@@ -56,7 +58,12 @@ void optionU(char* arg) {
         urlFile = "/";
 }
 
-void optionD() {
+void optionD() { // way to remove duplicates or is it not necessary
+    // Labels to lowercase
+    // transform(hostname.begin(), hostname.end(), hostname.begin(), ::tolower);
+    // transform(urlFile.begin(), urlFile.end(), urlFile.begin(), ::tolower);
+    // transform(filename.begin(), filename.end(), filename.begin(), ::tolower);
+
     cout << "DBG: host: " << hostname << endl;
     cout << "DBG: web_file: " << urlFile << endl;
     cout << "DBG: output_file: " << filename << endl;
@@ -76,14 +83,14 @@ void optionO() {
 
 // Process arguments from terminal
 int main(int argc, char* argv[]) {
-    parseArgs(argc, argv);
+    int urlIndex = parseArgs(argc, argv); // maybe urlIndex could be returned instead?
 
     optionU(argv[urlIndex]);
     if (dIndex != -1)
         optionD();
-    optionO();
     if (qIndex != -1)
         optionQ();
+    optionO();
     if (rIndex != -1)
         optionR();
 }

6eff86c3e312252770d850846027e277da53f025
chkim0820 <chaekim0820@gmail.com>
2023-09-29 14:55:54 -0400

    -d and -u works I think

--- Project 2/Project2.cpp
+++ Project 2/Project2.cpp
@@ -1,9 +1,6 @@
-/**
- * @file Project2.cpp
- * @author Chaehyeon Kim cxk445
- * @brief Completed for CSDS 325 Project 2
- */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <cstddef>
 #include <cstring>
 #include <iostream>
@@ -11,26 +8,55 @@
 #include <tuple>
 using namespace std;
 
-class Project2 {
-    private:
-        tuple<string, string, string> optionU();
-        void optionD();
-        void optionQ();
-        void optionR();
-        void optionO();
-};
+// Global variables
+int uIndex = -1;
+int oIndex = -1;
+int dIndex = -1;
+int qIndex = -1;
+int rIndex = -1;
+int urlIndex = -1;
+string url;
+string urlFile;
+string hostname;
+string filename;
 
-tuple<string, string, string> optionU(char* arg) {
-    string url = arg;
-    string hostname = strtok(arg + 7, "/");
-    string urlFile = url.substr(size(hostname) + 7, string::npos);
-    if (urlFile.empty())
-        urlFile = "/";
-    tuple<string, string, string> result = make_tuple(url, hostname, urlFile);
-    return result;
+void parseArgs(int argc, char* argv[]) {
+    for (int i = 1; i < argc; i++) { // go through all arguments
+        char* arg = argv[i];
+        if (0 == strncasecmp(arg, "-u", 2)) // account for diff. cases
+            uIndex = i;
+        else if (0 == strncasecmp(arg, "-o", 2))
+            oIndex = i;
+        else if (0 == strncasecmp(arg, "-d", 2))
+            dIndex = i;
+        else if (0 == strncasecmp(arg, "-q", 2))
+            qIndex = i;
+        else if (0 == strncasecmp(arg, "-r", 2))
+            rIndex = i;
+        else if (0 == strncasecmp(arg, "http://",7))
+            urlIndex = i;
+        else if (0 == strncasecmp(arg, "wg",2))// FIX
+            filename = "";
+        else{
+            cout << "Invalid argument " << arg << "\n";
+            exit(1);
+        }
+    }
+    if (uIndex == -1 || oIndex == -1 || argc < 4) {
+        cout << "Valid arguments required; ./proj2 -u URL [-d] [-q] [-r] -o filename\n";
+        exit(1); // wrong input; try again with 
+    }
 }
 
-void optionD(string urlFile, string hostname, string filename) {
+void optionU(char* arg) {
+    url = arg;
+    hostname = strtok(arg + 7, "/");
+    urlFile = url.substr(size(hostname) + 7, string::npos);
+    if (urlFile.empty())
+        urlFile = "/";
+}
+
+void optionD() {
     cout << "DBG: host: " << hostname << endl;
     cout << "DBG: web_file: " << urlFile << endl;
     cout << "DBG: output_file: " << filename << endl;
@@ -50,56 +76,14 @@ void optionO() {
 
 // Process arguments from terminal
 int main(int argc, char* argv[]) {
-    bool uPresent = false;
-    bool oPresent = false;
-    bool dPresent = false;
-    bool qPresent = false;
-    bool rPresent = false;
+    parseArgs(argc, argv);
 
-    string url;
-    string urlFile;
-    string hostname;
-    string filename;
-
-    for (int i = 1; i < argc; i++) {
-        string arg = argv[i];
-        if (0 == strncasecmp(argv[i], "-u", 2)) // account for diff. cases
-            uPresent = true;
-        else if (0 == strncasecmp(argv[i], "-o", 2))
-            oPresent = true;
-        else if (0 == strncasecmp(argv[i], "-d", 2))
-            dPresent = true;
-        else if (0 == strncasecmp(argv[i], "-q", 2))
-            qPresent = true;
-        else if (0 == strncasecmp(argv[i], "-r", 2))
-            rPresent = true;
-        else if (0 == strncasecmp(argv[i], "http://",7)){
-            uPresent = true;
-            tuple<string, string, string> result = optionU(argv[i]);
-            url = get<0>(result);
-            hostname = get<1>(result);
-            urlFile = get<2>(result);
-        }
-        else if (0 == strncasecmp(argv[i], "wg",2))// FIX
-            filename = "";
-        else{
-            cout << "Invalid argument " << argv[i] << "\n";
-            exit(1);
-        }
-    }
-    if (uPresent == false || oPresent == false || argc < 4) {
-        cout << "Valid arguments required; ./proj2 -u URL [-d] [-q] [-r] -o filename\n";
-        exit(1); // wrong input; try again with 
-    }
-    
-    if (dPresent)
-        optionD(urlFile, hostname, filename);
-
-    
+    optionU(argv[urlIndex]);
+    if (dIndex != -1)
+        optionD();
     optionO();
-    
-    if (qPresent)
+    if (qIndex != -1)
         optionQ();
-    if (rPresent)
+    if (rIndex != -1)
         optionR();
 }

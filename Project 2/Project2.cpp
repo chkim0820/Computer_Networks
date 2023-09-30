fc662f5e676f2ab6928cce91f5679c56a1e620ba
chkim0820 <chaekim0820@gmail.com>
2023-09-30 18:34:00 -0400

    changed u and d to use char

--- Project 2/Project2.cpp
+++ Project 2/Project2.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <sys/socket.h>
 
 using namespace std;
 
@@ -15,10 +16,17 @@ int oIndex = -1;
 int dIndex = -1;
 int qIndex = -1;
 int rIndex = -1;
-string url;
-string urlFile;
-string hostname;
-string filename;
+char* url = nullptr;
+char* urlFile = nullptr;
+char* hostname = nullptr;
+char* filename = nullptr;
+
+#define ERROR 1
+#define REQUIRED_ARGC 3
+#define HOST_POS 1
+#define PORT_POS 2
+#define PROTOCOL "tcp"
+#define BUFLEN 1024
 
 int parseArgs(int argc, char* argv[]) {
     int urlIndex = -1;
@@ -36,42 +44,81 @@ int parseArgs(int argc, char* argv[]) {
             rIndex = i;
         else if (0 == strncasecmp(arg, "http://",7))
             urlIndex = i;
-        else if (i == oIndex + 1)// FIX
-            filename = arg;
+        else if (i == oIndex + 1) { // FIX
+            filename = new char[strlen(arg)];
+            strcpy(filename, arg);
+        }
         else{
-            cout << "Invalid argument " << arg << "\n";
-            exit(1);
+            fprintf(stderr, "Invalid argument %s\n", arg);
+            exit(ERROR);
         }
     }
     if (uIndex == -1 || oIndex == -1 || argc < 5) { // FIX: check for url and filename, too?
-        cout << "Valid arguments required; ./proj2 -u URL [-d] [-q] [-r] -o filename\n";
-        exit(1); // wrong input; try again with 
+        fprintf(stderr, "Valid arguments required; ./proj2 -u URL [-d] [-q] [-r] -o filename\n");
+        exit(ERROR); // wrong input; try again with 
     }
     return urlIndex;
 }
 
+int errorExit (char *format, char *arg)
+{
+    fprintf (stderr,format,arg);
+    fprintf (stderr,"\n");
+    exit (ERROR);
+}
+
 void optionU(char* arg) {
-    url = arg;
-    hostname = strtok(arg + 7, "/");
-    urlFile = url.substr(size(hostname) + 7, string::npos);
-    if (urlFile.empty())
-        urlFile = "/";
+    // Safety feature; deallocate if previously allocated
+    if (url != nullptr)
+        delete[] url;
+    if (hostname != nullptr)
+        delete[] hostname;
+    if (urlFile != nullptr)
+        delete[] urlFile;
+
+    // Copying whole arg into url
+    url = new char[strlen(arg) + 1]; // Saves the whole url
+    strcpy(url, arg);
+
+    // Assigning hostname from arg
+    char* tok = strtok(arg + 7, "/"); // Skip "http://"
+    hostname = new char[strlen(tok) + 1];
+    if (tok != nullptr)
+        strcpy(hostname, tok);
+    else { // Nothing after "http://"
+        fprintf(stderr, "No valid hostname could be found");    
+        exit(ERROR);
+    }
+
+    // Assigning urlFile if exists
+    urlFile = new char[strlen(arg)];
+    strcpy(urlFile, url + strlen(arg));
+
+    if (urlFile[0] == '\0' || urlFile == nullptr)
+        strcpy(urlFile, "/");
 }
 
-void optionD() { // way to remove duplicates or is it not necessary
-    // Labels to lowercase
-    // transform(hostname.begin(), hostname.end(), hostname.begin(), ::tolower);
-    // transform(urlFile.begin(), urlFile.end(), urlFile.begin(), ::tolower);
-    // transform(filename.begin(), filename.end(), filename.begin(), ::tolower);
-
-    cout << "DBG: host: " << hostname << endl;
-    cout << "DBG: web_file: " << urlFile << endl;
-    cout << "DBG: output_file: " << filename << endl;
+void optionD() {
+    fprintf(stdout, "DBG: host: %s\n", hostname);
+    fprintf(stdout, "DBG: web_file: %s\n", urlFile);
+    fprintf(stdout, "DBG: output_file: %s\n", filename);
 }
+
 
 void optionQ() {
+    // int sd = socket(PF_INET, SOCK_STREAM, );
 
+
+    // string httpVer; 
+    // cout << "GET " << urlFile << " HTTP/" << httpVer << "\r\n";
+    // cout << "Host: " << hostname << "\r\n";
+    // cout << "User-Agent: CWRU CSDS 325 SimpleClient 1.0\r\n";
 }
+
+
+
+
+
 
 void optionR() {
 
@@ -88,6 +135,8 @@ int main(int argc, char* argv[]) {
     optionU(argv[urlIndex]);
     if (dIndex != -1)
         optionD();
+
+    // to implement:
     if (qIndex != -1)
         optionQ();
     optionO();

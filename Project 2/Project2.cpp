1060343cf3c56f46cf377527995eb0fb31c27a49
chkim0820 <chaekim0820@gmail.com>
2023-10-01 17:01:35 -0400

    All working now

--- Project 2/Project2.cpp
+++ Project 2/Project2.cpp
@@ -1,46 +1,58 @@
-#include <algorithm>
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * @file Project2.cpp
+ * @author Chaehyeon Kim cxk445
+ */
+
 #include <unistd.h>
-#include <cstddef>
 #include <cstring>
-#include <iostream>
-#include <string>
+#include <fstream>
 #include <sys/socket.h>
-#include <strings.h>
-#include <string.h>
 #include <netdb.h>
-#include <netinet/in.h>
 
 using namespace std;
 
-// Global variables
-int uIndex = -1;
-int oIndex = -1;
-int dIndex = -1;
-int qIndex = -1;
-int rIndex = -1;
+/* Define macros */
+#define COMPARE_ARG(arg, opt) (0 == strncasecmp(arg, opt, strlen(opt)))
+#define ERROR 1
+#define INT_ERROR -1
+#define HTTP_ERROR 200
+#define REQUIRED_ARGC 5
+#define PORT_NUM 80
+#define PROTOCOL "tcp"
+#define BUFLEN 1024
+
+/* Global variables */
+int uIndex = INT_ERROR;
+int oIndex = INT_ERROR;
+int dIndex = INT_ERROR;
+int qIndex = INT_ERROR;
+int rIndex = INT_ERROR;
 char* url = nullptr;
 char* urlFile = nullptr;
 char* hostname = nullptr;
 char* filename = nullptr;
 
-#define COMPARE_ARG(arg, opt) (0 == strncasecmp(arg, opt, strlen(opt)))
+/** Functions */
 
-#define ERROR 1
-#define REQUIRED_ARGC 3
-#define PORT_NUM 80
-#define PROTOCOL "tcp"
-#define BUFLEN 1024
-
-int errorExit (const char *format, const char *arg) {
+/**
+ * @brief Prints out error messages when called; got from sample.c
+ * @param format Given format of the message to be printed
+ * @param arg Replaced with the placeholder; NULL if none
+ */
+void errorExit (const char *format, const char *arg) {
     fprintf (stderr, format, arg);
     fprintf (stderr, "\n");
     exit (ERROR);
 }
 
+/**
+ * @brief Parse arguments from the main methods
+ * @param argc Number of arguments
+ * @param argv Array containing arguments
+ * @return int integer value of url index
+ */
 int parseArgs(int argc, char* argv[]) {
-    int urlIndex = -1;
+    int urlIndex = INT_ERROR;
     for (int i = 1; i < argc; i++) { // go through all arguments
         char* arg = argv[i];
         if COMPARE_ARG(arg, "-u")
@@ -62,11 +74,15 @@ int parseArgs(int argc, char* argv[]) {
         else
             errorExit("Invalid argument %s", arg);
     }
-    if (uIndex == -1 || oIndex == -1 || argc < 5)
+    if (uIndex == INT_ERROR || oIndex == INT_ERROR || argc < REQUIRED_ARGC) // if -u or -o not found or less than 5 args
         errorExit("Valid arguments required:\n./proj2 -u URL [-d] [-q] [-r] -o filename", NULL);
     return urlIndex;
 }
 
+/**
+ * @brief Creating socket and sending http request & receiving http response; modified from sockets.c
+ * @return string http response
+ */
 string httpConnect() {
     struct sockaddr_in sin;
     struct hostent *hinfo;
@@ -74,32 +90,30 @@ string httpConnect() {
     char buffer [BUFLEN];
     int sd, ret;
 
-    /* lookup the hostname */
+    //lookup the hostname
     hinfo = gethostbyname(hostname);
     if (hinfo == NULL)
         errorExit("cannot find name: %s", hostname);
 
-    /* set endpoint information */
+    // set endpoint information
     memset((char*)&sin, 0x0, sizeof(sin)); // for memory
     sin.sin_family = AF_INET;
     sin.sin_port = htons(PORT_NUM); // for http
     memcpy ((char *)&sin.sin_addr, hinfo->h_addr, hinfo->h_length);
-
     if ((procinfo = getprotobyname(PROTOCOL)) == NULL)
         errorExit ("cannot find protocol information for %s", PROTOCOL);
 
-    /* allocate a socket */
-    /*   would be SOCK_DGRAM for UDP */
+    // allocate a socket
     sd = socket(PF_INET, SOCK_STREAM, procinfo->p_proto);
     if (sd < 0)
         errorExit("cannot create socket",NULL);
 
-    /* connect the socket */
+    // connect the socket
     if (connect(sd, (struct sockaddr *)&sin, sizeof(sin)) < 0)
         errorExit("cannot connect", NULL);
 
-    /* Snd an HTTP request */
-    char http_request[BUFLEN];
+    // Send an HTTP request
+    char http_request[BUFLEN]; // saves http_request
     snprintf(http_request, sizeof(http_request), 
         "GET %s HTTP/1.0\r\n"
                 "Host: %s\r\n"
@@ -109,18 +123,22 @@ string httpConnect() {
     if (send(sd, http_request, strlen(http_request), 0) < 0)
         errorExit("cannot send", NULL);
 
-    /* snarf whatever server provides and print it */
+    // snarf whatever server provides and print it
     memset(buffer,0x0,BUFLEN);
     ret = read(sd,buffer,BUFLEN - 1);
     if (ret < 0)
         errorExit("reading error",NULL);
 
-    /* close & return buffer */
+    // close & return http response
     close(sd);
-    string retBuffer = buffer;
-    return retBuffer;
+    string httpResponse = buffer;
+    return httpResponse;
 }
 
+/**
+ * @brief Executes option U when called; assigns appropriate variables 
+ * @param arg url followed by -u in the input
+ */
 void optionU(char* arg) {
     // Safety feature; deallocate if previously allocated
     if (url != nullptr)
@@ -130,31 +148,39 @@ void optionU(char* arg) {
     if (urlFile != nullptr)
         delete[] urlFile;
 
+    // Allocating memory
+    url = new char[strlen(arg) + 1];
+    hostname = new char[strlen(arg)];
+    urlFile = new char[strlen(arg)];
+
     // Copying whole arg into url
-    url = new char[strlen(arg) + 1]; // Saves the whole url
     strcpy(url, arg);
 
     // Assigning hostname from arg
     char* tok = strtok(arg + 7, "/"); // Skip "http://"
-    hostname = new char[strlen(tok) + 1];
     if (tok != nullptr)
         strcpy(hostname, tok);
     else // nothing after 'http://'
         errorExit("No valid hostname could be found", NULL); 
 
-    // Assigning urlFile if exists
-    urlFile = new char[strlen(arg)];
+    // Assigning urlFile if exists (optional)
     strcpy(urlFile, url + strlen(arg));
     if (urlFile[0] == '\0' || urlFile == nullptr)
         strcpy(urlFile, "/");
 }
 
+/**
+ * @brief Executes option D when called; prints out hostname, web_file, and output_file
+ */
 void optionD() {
     fprintf(stdout, "DBG: host: %s\n", hostname);
     fprintf(stdout, "DBG: web_file: %s\n", urlFile);
     fprintf(stdout, "DBG: output_file: %s\n", filename);
 }
 
+/**
+ * @brief Executes option Q when called; printing http request
+ */
 void optionQ() {
     // Printing outputs //FIX so that OUT: is directly added to the actual input
     fprintf(stdout, "OUT: GET %s HTTP/1.0\r\n", urlFile); //FIX
@@ -162,38 +188,61 @@ void optionQ() {
     fprintf(stdout, "OUT: User-Agent: CWRU CSDS 325 SimpleClient 1.0\r\n");
 }
 
-void optionR(const char* buffer) {
-    /* Creating a temporary buffer and cutting everything out after "\r\n\r\n" */
-    char* temp = new char[strlen(buffer) + 1];
-    strcpy(temp, buffer); 
-    char* stopping = strstr(temp, "\r\n\r\n");
+/**
+ * @brief Executes option R when called; printing http response
+ * @param response received http response
+ */
+void optionR(const char* response) {
+    // Creating a temporary variable containing everything after "\r\n\r\n"; strtok can't differentiate duplicated \r\n
+    char* tempResp = new char[strlen(response) + 1]; // allocate memory
+    strcpy(tempResp, response); // copy over original response
+    char* stopping = strstr(tempResp, "\r\n\r\n"); // everything after empty line; stopping point
     for (int i = 0; *stopping && i < 4; i++) // go to the first character after "\r\n\r\n"
         stopping++;
-    temp[strlen(temp) - strlen(stopping)] = '\0'; // char first one after stopping
+    tempResp[strlen(tempResp) - strlen(stopping)] = '\0'; // char first one after stopping
+
     // Output line by line
-    char* line = strtok(temp, "\r\n");
+    char* line = strtok(tempResp, "\r\n");
     while (line != nullptr) { // until empty line with only "\r\n"
-        fprintf(stdout, "INC: %s\n", line);
+        fprintf(stdout, "INC: %s\r\n", line);
         line = strtok(nullptr, "\r\n");
     }
 }
 
-void optionO() {
+/**
+ * @brief Executes option O when called; saves contents to filename
+ * @param response received http response
+ */
+void optionO(string response) {
+    int start = response.find("\r\n") + 4; // find the empty line & skip over that line
+    string downloaded = response.substr(start, string::npos); // after empty line to end
+    int errCode = stoi(response.substr(9, 3)); // fetch error code
+    if (errCode != HTTP_ERROR)
+        errorExit("ERROR: non-200 response code\n%s: No such file or directory", filename);
 
+    // Save contents to designated file name
+    ofstream toFile(filename);
+    toFile << downloaded;
+    toFile.close();
 }
 
-// Process arguments from terminal
+/**
+ * @brief The main method 
+ * @param argc number of arguments
+ * @param argv array containing arguments
+ * @return int returns int
+ */
 int main(int argc, char* argv[]) {
     int urlIndex = parseArgs(argc, argv); // maybe urlIndex could be returned instead?
 
     optionU(argv[urlIndex]);
-    if (dIndex != -1)
+    if (dIndex != INT_ERROR)
         optionD();
 
-    string buffer = httpConnect();
-    if (qIndex != -1)
+    string httpResponse = httpConnect();
+    if (qIndex != INT_ERROR)
         optionQ();
-    if (rIndex != -1)
-        optionR(buffer.c_str());
-    optionO();
+    if (rIndex != INT_ERROR)
+        optionR(httpResponse.c_str());
+    optionO(httpResponse);
 }

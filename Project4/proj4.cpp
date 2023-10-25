00ec95a3a2bd5ec9b5dc9b542da203cc6baf5f3d
chkim0820 <chaekim0820@gmail.com>
2023-10-25 14:08:25 -0400

    Getting started with project 4

--- Project4/proj4.cpp
+++ Project4/proj4.cpp
@@ -1,6 +1,11 @@
 /**
 * @file proj4.cpp
 * @author Chaehyeon Kim (cxk445)
-* @brief Script for CSDS 325 HW4
+* @brief Script for CSDS 325 HW4; working with packets
 * @date 2023-10-25
 */
+
+
+int main(int argc, char* argv[]) {
+    
+}

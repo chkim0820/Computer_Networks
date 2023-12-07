9bcd494dc5a91ab7863a23b2f9e5c6d0a5503f02
chkim0820 <chaekim0820@gmail.com>
2023-12-07 12:42:23 -0500

    python code updated

--- Project5/dataProcessor.py
+++ Project5/dataProcessor.py
@@ -3,14 +3,40 @@
 # Description: For CSDS 325 Project 5; gathering measurements for different networks
 # Date: 11/25/2023
 
-import subprocess
+import os
 
-# Write code for collecting data here
+# A list of websites the network traffics lead to
+websiteList = ["Amazon", "Canvas", "Case", "Google", "Instagram", "Youtube"]
 
+def openFile(fileType, destName="", network=""):
+    folderDir = os.path.dirname(__file__) # Directory to the current script
+    fileName = fileType + destName + network + ".txt"
+    print(fileName)
+    exit()
+    absFileDir = os.path.join(folderDir, fileName) # Absolute path of the file
+    file = open(absFileDir, "r") # Contains the output
+    return file
+
+# Process the ping data files for all 6 sites, each for Spectrum and CaseWireless
+def processPingData(network):
+    for website in websiteList:
+        openFile("ping", website)
+
+def processIPerfData():
+    print("iperf")
+
+def processTraceRouteData():
+    print("route data")
+
+def processNetstatData():
+    print("netstat")
 
 
 
 if __name__ == '__main__':
-    # Run the data collection bash script
-    subprocess.run(["bash", "dataCollector.sh"])
+    for network in ["Spectrum", "CaseWireless"]:
+        processPingData(network)
+    processIPerfData()
+    processTraceRouteData()
+    processNetstatData()
 

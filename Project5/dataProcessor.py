35a047794e14738bc27f3251c59cc476f6869fe8
chkim0820 <chaekim0820@gmail.com>
2023-12-08 14:43:32 -0500

    got rid of some numbers

--- Project5/dataProcessor.py
+++ Project5/dataProcessor.py
@@ -10,14 +10,19 @@ import matplotlib.pyplot as plt
 
 # A list of websites the network traffics lead to
 websiteList = ["Amazon", "Canvas", "Case", "Google", "Instagram", "Youtube"]
+# Dictionary to full domains of each website name
 fullDomain = {"Amazon": "amazon.com", 
               "Canvas": "canvas.case.edu", 
               "Case": "case.edu", 
               "Google": "google.com", 
               "Instagram": "instagram.com", 
               "Youtube": "youtube.com"}
+# A list of types of measurements (7 total)
+measurements = ["Round Trip Time", "Packet Loss", "Jitter", "Number of Hops", "Throughput", "Bandwidth", "Retransmission"]
 # Color map for plotting 6 different websites
 colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:pink', 'tab:purple', 'tab:olive']
+# Total number of data points
+totalData = 1000
 
 # A function to open the specified file containing data
 def openFile(fileType, destName="", network=""):
@@ -39,32 +44,35 @@ def setEmptyLists(dataframes):
 # Process the ping data files for all 6 sites, each for Spectrum and CaseWireless
 def processPingData(network, dataframe):
     rttInfo = [] # To include provided information about RTTs
+    rttOrder = measurements.index("Round Trip Time")
+    jitterOrder = measurements.index("Jitter")
+    lossOrder = measurements.index("Packet Loss")
     # Go through files for all websites
     for website in websiteList:
         lines = openFile("ping", website, network) # Open the appropriate file
         totalLines = len(lines)
         prevRTT = -1
         # Iterating through each lines except beginning & summary
-        for i in range(1, totalLines-4):
+        for i in range(1, totalLines - 4):
             line = lines[i] # Current line
             # RTT
-            rttIndex = line.find("time=") + 5 # Index of where RTT value starts
+            rttIndex = line.find("time=") + len("time=") # Index of where RTT value starts
             rttEnd = line.find("ms") # Index of where RTT value ends
             rtt = float(line[rttIndex: rttEnd])
-            dataframe.iloc[i-1, 0].append(rtt) # Add RTT value to dataframe
+            dataframe.iloc[i - 1, rttOrder].append(rtt) # Add RTT value to dataframe
             # Jitter
             jitter = abs(prevRTT - rtt) if (i != 1) else 0 # Jitter 0 if no prev. RTT
-            dataframe.iloc[i-1, 2].append(jitter) # Adding calculated jitter
+            dataframe.iloc[i - 1, jitterOrder].append(jitter) # Adding calculated jitter
             prevRTT = rtt
         # Fill in for missing data/packets (add None as placeholder)
-        for i in range(1000 - (totalLines - 5)):
-            dataframe.iloc[999-i, 0].append(None)
-            dataframe.iloc[999-i, 2].append(None)
+        for i in range(totalData - (totalLines - 5)):
+            dataframe.iloc[totalData - 1 - i, rttOrder].append(None)
+            dataframe.iloc[totalData - 1 - i, jitterOrder].append(None)
         # Add the website's packet loss rate
         summaryLine = lines[totalLines - 2]
         recIndex = summaryLine.find("transmitted,") + len("transmitted,")
         recEnd = summaryLine.find("received")
-        dataframe.iloc[0, 1].append(summaryLine[recIndex: recEnd])
+        dataframe.iloc[0, lossOrder].append(summaryLine[recIndex: recEnd])
         # Save the additional RTT info provided at the end
         rttLine = lines[totalLines - 1]
         rttInd = rttLine.find("= ") + len("= ")
@@ -75,33 +83,36 @@ def processPingData(network, dataframe):
 # Process the iperf data for each network 
 def processIPerfData(network, dataframe):
     lines = openFile("iperf", network=network) # File containing iperf data
+    throughputOrder = measurements.index("Throughput")
+    bandwidthOrder = measurements.index("Bandwidth")
     # Iterate through all lines after basic information
-    for i in range(6, len(lines)-1):
+    for i in range(6, len(lines) - 1):
         line = lines[i]
         # Throughput
         thIndex = line.find("sec") + len("sec")
         thEnd = line.find("GBytes") 
         throughput = float(line[thIndex: thEnd])
-        dataframe.iloc[i-6, 4] = throughput # Save for the nth iteration
+        dataframe.iloc[i - 6, throughputOrder] = throughput # Save for the nth iteration
         # Bandwidth
         bwIndex = line.find("GBytes") + len("GBytes")
         bwEnd = line.find("Gbits/sec") 
         bandwidth = float(line[bwIndex: bwEnd])
-        dataframe.iloc[i-6, 5] = bandwidth # Save for the nth iteration
+        dataframe.iloc[i - 6, bandwidthOrder] = bandwidth # Save for the nth iteration
         
 # Process the traceroute data for each website
 def processTraceRouteData(network, dataframe):
+    maxHopOrder = measurements.index("Number of Hops")
     for website in websiteList:
         lines = openFile("tr", website, network) # traceroute file
         lineIt = 0 # Total number of lines iterated
         maxHop = 0 # Max number of hops for each traceroute command
         traceN = 0 # The nth iteration of separate traceroute commands
         # Traverse the data until 1000 entries are filled
-        while (traceN < 1000):
+        while (traceN < totalData):
             line = lines[lineIt]
             if (line.find("traceroute to") != -1): # If new traceroute command started
                 if (lineIt != 0): # If not the first line
-                    dataframe.iloc[traceN, 3].append(maxHop)
+                    dataframe.iloc[traceN, maxHopOrder].append(maxHop)
                     traceN += 1
             else: # Still traversing through the same traceroute command results
                 numTime = int(line[0: 2]) # Number at the front of the line
@@ -126,7 +137,7 @@ def processNetstatData(network, dataframe):
             totalResend = int(line[0: totalResendInd])
             break # No longer need to traverse the lines
     retransmissionRate = totalResend / totalSent # Calculating retransmission rate
-    dataframe.iloc[0, 6] = [retransmissionRate, totalResend, totalSent]
+    dataframe.iloc[0, measurements.index("Retransmission")] = [retransmissionRate, totalResend, totalSent]
 
 # Calculating minimum, maximum, average, and standard deviation of measurement data
 def calculateStats(data, numCols):
@@ -172,13 +183,13 @@ def plotPlots(network, data, measurement, unit, numCols=6, individual=False):
             plt.xlabel(f"ith {measurement} Request")
             plt.ylabel(f"{measurement} in {unit}")
             plt.plot(x, y, color=colors[website])
-            plt.savefig(f"{network}_{siteName}_{measurement}_Plot")
-            # plt.show()
+            # plt.savefig(f"{network}_{siteName}_{measurement}_Plot")
+            plt.show()
         else: # Add to the plot if not showing
             plt.plot(x, y, label=f"{siteName}", color=colors[website])
     plt.legend()
-    plt.savefig(f"{network}_{measurement}_Plot", bbox_inches="tight")
-    # plt.show()
+    # plt.savefig(f"{network}_{measurement}_Plot", bbox_inches="tight")
+    plt.show()
 
 # Creating a table with the input data
 def plotTable(network, data, measurement, unit, numCols=6, dataParsed=False):
@@ -192,12 +203,12 @@ def plotTable(network, data, measurement, unit, numCols=6, dataParsed=False):
     elif (numCols==1):
         plt.table(cellText=[valueTypes, data[0]], loc='center')
     plt.axis('off')
-    plt.savefig(f"{network}_{measurement}_Table", bbox_inches="tight")
-    # plt.show()
+    # plt.savefig(f"{network}_{measurement}_Table", bbox_inches="tight")
+    plt.show()
 
 # Plotting plots and graphs for RTT
 def plotRTT(network, dataframe, rttInfo):
-    plotPlots(network, dataframe["Round Trip Time"], "Round Trip Time", "ms", individual=True)
+    plotPlots(network, dataframe["Round Trip Time"], "Round Trip Time", "ms")
     # Shifting around columns (min, avg, max) to (avg, min, max)
     for row in rttInfo:
         avg = row[1]
@@ -209,7 +220,7 @@ def plotRTT(network, dataframe, rttInfo):
 def plotPacketLoss(network, dataframe):
     plt.clf() # Clearing the plot
     # Creating the list to create the table with
-    packetsSent = [1000] * len(websiteList)
+    packetsSent = [totalData] * len(websiteList)
     packetsLost = dataframe.iloc[0, 1]
     for i in range(len(packetsLost)): # Converting values from string to int
         packetsLost[i] = int(packetsLost[i])
@@ -222,31 +233,31 @@ def plotPacketLoss(network, dataframe):
     plt.table(cellText=data, rowLabels=valueTypes, colLabels=websiteList, loc='center')
     plt.title(f"Packet Loss of {network}")
     plt.axis('off')
-    plt.savefig(f"{network}_PacketLoss_Table", bbox_inches="tight")
-    # plt.show()
+    # plt.savefig(f"{network}_PacketLoss_Table", bbox_inches="tight")
+    plt.show()
 
 # Plotting plots and graphs for jitter
 def plotJitter(network, dataframe):
     data = dataframe["Jitter"].iloc[1:len(dataframe)] # Exclude the first row
-    plotPlots(network, data, "Jitter", "ms", individual=True)
+    plotPlots(network, data, "Jitter", "ms")
     # plotTable(network, data, "Jitter", "ms")
 
 # Plotting plots and graphs for number of hops
 def plotHops(network, dataframe):
     data = dataframe["Number of Hops"]
-    plotPlots(network, data, "Number of Hops", "Total", individual=True)
+    plotPlots(network, data, "Number of Hops", "Total")
     # plotTable(network, data, "Number of Hops", "Total")
 
 # Plotting plots and graphs for throughput
 def plotThroughput(network, dataframe):
     data = dataframe["Throughput"]
-    plotPlots(network, data, "Throughput", "GBytes", numCols=1, individual=True)
+    plotPlots(network, data, "Throughput", "GBytes", numCols=1)
     # plotTable(network, data, "Throughput", "GBytes", numCols=1)
 
 # Plotting plots and graphs for bandwidth
 def plotBandwidth(network, dataframe):
     data = dataframe["Bandwidth"]
-    plotPlots(network, data, "Bandwidth", "Gbits/sec", numCols=1, individual=True)
+    plotPlots(network, data, "Bandwidth", "Gbits/sec", numCols=1)
     # plotTable(network, data, "Bandwidth", "Gbits/sec", numCols=1)
 
 # Plotting plots and graphs for retransmission rate
@@ -257,16 +268,15 @@ def plotRetransmission(network, dataframe):
     plt.table(cellText=[valueTypes, data], loc='center')
     plt.title(f"Retransmission Rate of {network}")
     plt.axis('off')
-    plt.savefig(f"{network}_Retransmission_Table", bbox_inches="tight")
-    # plt.show()
+    # plt.savefig(f"{network}_Retransmission_Table", bbox_inches="tight")
+    plt.show()
 
 # The main function
 if __name__ == '__main__':
     # Run dataCollector.sh separately
     # Initializing the dataframes for both Spectrum and CaseWireless
-    columns = ["Round Trip Time", "Packet Loss", "Jitter", "Number of Hops", "Throughput", "Bandwidth", "Retransmission"] # Types of measurements
-    spectrumData = pd.DataFrame(index=np.arange(1000), columns=columns) # Number of rows preset to 1000 & columns as measurements
-    CWData = pd.DataFrame(index=np.arange(1000), columns=columns) # Same as above but for CaseWireless data
+    spectrumData = pd.DataFrame(index=np.arange(totalData), columns=measurements) # Number of rows preset to 1000 & columns as measurements
+    CWData = pd.DataFrame(index=np.arange(totalData), columns=measurements) # Same as above but for CaseWireless data
     setEmptyLists([spectrumData, CWData])
 
     # Iterating through the data and adding to the appropriate dataframes
